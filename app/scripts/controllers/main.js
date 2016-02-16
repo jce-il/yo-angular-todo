f482c3051e0426a97a0072d90448754c7a78bae2
Amrith Yerramilli <amrith.y@deltax.com>
2016-02-16 23:44:39 +0530

    initial commit
    * default yeoman starter app
    * fixed dependency errors with karma, jasmine, phantom
    * fixed grunt test error due to port 8080 (replaced with 9001)
    * not sure if travis config is correct (TODO)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularTodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAngularTodoApp
+ */
+angular.module('yoAngularTodoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
